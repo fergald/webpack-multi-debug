d6ec3df250e3dedf52efb619a1be16aaf9722cfe
Fergal Daly <fergald@gmail.com>
2022-04-03 17:06:27 +0900

    add files

---
+++ js/lib2.js
@@ -0,0 +1,1 @@
+export const d2 = "hello2";
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./js/lib1.js', './js/lib2.js'],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    library: {
+      name: 'MyLibrary',
+      type: 'var',
+    },
+    filename: 'lib.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  "optimization": {
+    "minimize": false,
+    usedExports: true,
+  },
+  mode: "development",
+};
